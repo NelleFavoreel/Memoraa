ac62446284f223451535d9395efc7382036dfd71
NelleFavoreel <nelle.favoreel@student.ehb.be>
2025-05-12 10:08:11 +0200

    Add notifications page and link in navbar

---
+++ frontend/src/pages/Notifications.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FamilyRequests from "../components/notifications/FamilyRequests";
+
+function Notifications() {
+  return (
+    <div>
+      <h2>Notificaties</h2>
+      <FamilyRequests />
+    </div>
+  );
+}
+
+export default Notifications;
