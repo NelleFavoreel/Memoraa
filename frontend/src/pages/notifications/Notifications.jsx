c3328885716e8318dd88db7fffa8eab3b1453eb9
NelleFavoreel <nelle.favoreel@student.ehb.be>
2025-06-02 14:56:46 +0200

    Refactor Notifications component for improved styling and animation; update CSS for better layout and responsiveness

--- frontend/src/pages/notifications/Notifications.jsx
+++ frontend/src/pages/notifications/Notifications.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FamilyRequests from "../../components/notifications/FamilyRequests";
 import GetNotifications from "../../components/notifications/GetNotifications";
 import "./Notifications.css";
 
 function Notifications() {
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    setAnimate(true);
+  }, []);
+
   return (
     <div>
-      <h1 className="title">Notificaties</h1>
+      <h1 className={`title ${animate ? "animate" : ""}`}>Meldingen</h1>
       <div className="notifications-container">
         <FamilyRequests />
-        <GetNotifications></GetNotifications>
+        <GetNotifications />
       </div>
     </div>
   );
