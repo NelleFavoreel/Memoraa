b0ccf6373457698c306d2d16fc4537e5b536e2cc
NelleFavoreel <nelle.favoreel@student.ehb.be>
2025-06-09 16:52:14 +0200

    Update backend middleware to support URL-encoded data; adjust EditTrip component height for better responsiveness

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,9 @@ const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.static("public"));
 app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.static("public"));
 
 const tripsRoutes = require("./routes/trips");
 app.use("/trips", tripsRoutes);
